a405aba3d101afe5bd9c1be8a9b17c702023d672
AdarshYadav-dev <adarsh98215@gmail.com>
2025-11-05 23:33:27 +0530

    React Target Audience Layout (incomplete)

---
+++ Ui-project/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,13 @@
+
+const LeftContent = () => {
+    return (
+        <div className='h-full w-1/4 bg-blue-600'>
+            <div></div>
+            <div>
+                <i className="ri-arrow-right-up-line"></i>
+            </div>
+        </div>
+    )
+}
+
+export default LeftContent;
---
+++ Ui-project/src/components/Section2/Section2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Section2 = () => {
+    return (
+        <div>
+            <h1 className='bg-blue-500 h-screen w-full'>This is section 2 vala page</h1>
+        </div>
+
+    )
+}
+
+export default Section2
